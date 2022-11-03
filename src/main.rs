8e81646313f48ac8d2d7e18ca3d0efbbd409b5ac
Tom Schammo <tom@tomschammo.com>
2022-11-04 00:53:05 +0100

    feat: Add notifications over dbus

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
 use clap::Parser;
+use notify_rust::Notification;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about=None)]
 struct Args {
     #[clap(short, long, help = "TBA")]
     time: String,
+}
+
+
+/// Sends a notification to the dbus that the specified time has elapsed.
+///
+/// Optionally an icon can be provided. If not the 'alarm-timer' icon
+/// will be used as a fallback.
+fn display_notification(time: &str, icon: Option<&str>) {
+    let icon: &str = if let Some(icon) = icon {
+        icon
+    } else {
+        "alarm-timer"
+    };
+
+    #[cfg(debug_assertions)]
+    println!("Using icon: {}", icon);
+
+    Notification::new()
+        .summary("Time's up!")
+        .body(format!("Your timer of {} has eneded!", time).as_str())
+        .icon(icon)
+        .show()
+        .unwrap();
 }
 
 fn main() {
