0725930d0e94b6240a95ba47a496a4cb02e8f6fe
Tom Schammo <tom@tomschammo.com>
2022-11-04 12:43:47 +0100

    feat: Add function to parse the input string
    Add function to parse the input string and convert it into a `u64`,
    representing the time in seconds that the timer will run for.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,65 @@ struct Args {
     time: String,
 }
 
+/// format: <a>d<b>h<c>m<e>s, where a,b,c,e are numbers
+fn parse_time(time: &str) -> u64 {
+    // total seconds
+    let mut seconds: u64 = 0;
+
+    // current number that is parsed
+    let mut number: String = String::default();
+
+    // true if the function expects the next
+    // character to be a number. False otherwise.
+    let mut should_be_number = true;
+
+    for c in time.chars() {
+        if c.is_numeric() {
+            if should_be_number {
+                should_be_number = false;
+                number = String::default();
+            }
+            number.push(c);
+        } else if should_be_number {
+            unimplemented!("should not happen");
+        } else {
+            should_be_number = true;
+            let factor: u64 = match c {
+                'd' => {
+                    #[cfg(debug_assertions)]
+                    println!("{number} days");
+
+                    86_400
+                }
+
+                'h' => {
+                    #[cfg(debug_assertions)]
+                    println!("{number} hours");
+
+                    3_600
+                }
+                'm' => {
+                    #[cfg(debug_assertions)]
+                    println!("{number} minutes");
+
+                    60
+                }
+                's' => {
+                    #[cfg(debug_assertions)]
+                    println!("{number} seconds");
+
+                    1
+                }
+                _ => {
+                    unimplemented!("todo invalid character")
+                }
+            };
+            seconds += number.parse::<u64>().unwrap() * factor;
+        }
+    }
+
+    seconds
+}
 
 /// Sends a notification to the dbus that the specified time has elapsed.
 ///
